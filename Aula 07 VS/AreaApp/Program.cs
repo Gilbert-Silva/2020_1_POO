f82597029bc85cefa649afd904b48872932bb529
Gilbert Azevedo <gilbert.azevedo@academico.ifrn.edu.br>
2020-10-15 09:26:02 -0300

    Biblioteca

---
+++ Aula 07 VS/AreaApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AreaLib;
+
+namespace AreaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Area.AreaTriangulo(10, 20);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula 07 VS/AreaLib/Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaLib
+{
+    public class Area
+    {
+        public static double AreaTriangulo(double b, double h)
+        {
+            double area = b * h / 2;
+            return area;
+        }
+    }
+}
